6216fee6be7d871836eda9a9cc1ad1e883d88e2d
Nathan Helmig <nhelmig93@gmail.com>
2026-04-17 22:01:08 -0500

    Add pane create/kill commands and bounds checking for pane switching

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module gomux
 
 go 1.26.1
 
-require github.com/creack/pty v1.1.24 // indirect
+require (
+	github.com/creack/pty v1.1.24 // indirect
+	golang.org/x/sys v0.43.0 // indirect
+	golang.org/x/term v0.42.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
-	"io"
 	"os"
 	"os/exec"
+
+	"golang.org/x/term"
 )
 
 func main() {
+	// Put terminal in raw mode
+	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		panic(err)
+	}
+	defer term.Restore(int(os.Stdin.Fd()), oldState)
+
 	pane1, _ := NewPane(exec.Command("/bin/sh"))
 	pane2, _ := NewPane(exec.Command("/bin/sh"))
 	defer pane1.Close()
@@ -18,13 +27,42 @@ func main() {
 	win.AddPane(pane1)
 	win.AddPane(pane2)
 
-	go io.Copy(os.Stdout, win.Active.PTY.TTY)
+	switchOutput := make(chan *Pane)
+	go copyOutput(switchOutput)
+	switchOutput <- win.Active
 
-	fmt.Println("Ctrl+A 1/2 to switch panes, Ctrl+A q to quit")
-	runInputLoop(win)
+	fmt.Println("Ctrl+A 1/2=switch c=create x=kill q=quit")
+	runInputLoop(win, switchOutput)
 }
 
-func runInputLoop(win *Window) {
+func copyOutput(switchPane <-chan *Pane) {
+	var cancel func()
+	for {
+		newPane := <-switchPane
+		if cancel != nil {
+			cancel()
+		}
+		ctx, cncl := context.WithCancel(context.Background())
+		cancel = cncl
+		go func(p *Pane) {
+			buf := make([]byte, 1024)
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				default:
+					n, err := p.PTY.TTY.Read(buf)
+					if err != nil {
+						return
+					}
+					os.Stdout.Write(buf[:n])
+				}
+			}
+		}(newPane)
+	}
+}
+
+func runInputLoop(win *Window, switchOutput chan<- *Pane) {
 	prefixMode := false
 	reader := bufio.NewReader(os.Stdin)
 
@@ -32,12 +70,28 @@ func runInputLoop(win *Window) {
 		select {
 		case <-win.Active.Exited:
 			fmt.Printf("\n[Pane %d exited]\n", win.Active.ID)
-			return
+			win.Active.Close()
+			if len(win.Panes) == 1 {
+				return
+			}
+			// Remove exited pane and switch to another
+			for i, p := range win.Panes {
+				if p == win.Active {
+					win.Panes = append(win.Panes[:i], win.Panes[i+1:]...)
+					break
+				}
+			}
+			win.Active = win.Panes[0]
+			switchOutput <- win.Active
 		default:
 			ch, _ := reader.ReadByte()
 			if prefixMode {
-				if handlePrefixCommand(ch, win) {
+				pane, quit := handlePrefixCommand(ch, win)
+				if quit {
 					return
+				}
+				if pane != nil {
+					switchOutput <- pane
 				}
 				prefixMode = false
 			} else if ch == 1 {
@@ -49,18 +103,44 @@ func runInputLoop(win *Window) {
 	}
 }
 
-func handlePrefixCommand(ch byte, win *Window) bool {
+func handlePrefixCommand(ch byte, win *Window) (*Pane, bool) {
 	switch ch {
 	case '1':
-		win.SetActivePane(win.Panes[0])
-		fmt.Printf("\n[Switched to pane %d]\n", win.Active.ID)
+		if len(win.Panes) >= 1 {
+			win.SetActivePane(win.Panes[0])
+			fmt.Printf("\n[Switched to pane %d]\n", win.Active.ID)
+			return win.Active, false
+		}
 	case '2':
-		win.SetActivePane(win.Panes[1])
-		fmt.Printf("\n[Switched to pane %d]\n", win.Active.ID)
+		if len(win.Panes) >= 2 {
+			win.SetActivePane(win.Panes[1])
+			fmt.Printf("\n[Switched to pane %d]\n", win.Active.ID)
+			return win.Active, false
+		}
+	case 'c':
+		pane, _ := NewPane(exec.Command("/bin/sh"))
+		win.AddPane(pane)
+		fmt.Printf("\n[Created pane %d]\n", pane.ID)
+		return win.Active, false
+	case 'x':
+		win.Active.Close()
+		fmt.Printf("\n[Killed pane %d]\n", win.Active.ID)
+		// Remove killed pane from window
+		for i, p := range win.Panes {
+			if p == win.Active {
+				win.Panes = append(win.Panes[:i], win.Panes[i+1:]...)
+				break
+			}
+		}
+		if len(win.Panes) > 0 {
+			win.Active = win.Panes[0]
+			return win.Active, false
+		}
+		return nil, true
 	case 'q':
-		return true
+		return nil, true
 	default:
 		win.Active.PTY.TTY.Write([]byte{1, ch})
 	}
-	return false
+	return nil, false
 }
